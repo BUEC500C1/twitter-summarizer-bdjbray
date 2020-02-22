0f76788c7953148af532f0308007a0ec3d904d3d
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-02-21 19:25:55 -0500

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -1,5 +1,13 @@
-import pytest
 import main
 
 def testmain():	
 	assert main.test('Boston')==1
+
+def testmain2():	
+	assert main.test('LasVegas')==1
+
+def testmain3():	
+	assert main.test('Miami')==1
+
+def testmain4():	
+	assert main.test('abc')==0
